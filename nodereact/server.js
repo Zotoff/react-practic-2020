1abdbc5ac5e1090afc0e178b1179499a2162bb79
Zotoff <paulzotoff@yandex.ru>
2020-06-29 00:06:38 +0300

    Working with mongo

--- nodereact/server.js
+++ nodereact/server.js
@@ -1,14 +1,30 @@
 const express = require('express');
-
+const {MongoClient} = require('mongodb');
 const app = express();
 
+const url = 'mongodb://localhost:27017';
+
+
+
+
 app.get('/api/users', (req, res)=>{
-    res.json([
-        {
-            name: 'Paul',
-            lastname: 'Zotov'
+    // res.json([
+    //     {
+    //         name: 'Paul',
+    //         lastname: 'Zotov'
+    //     }
+    // ])
+    MongoClient.connect(url,{useUnifiedTopology: true},(err, client)=>{
+        if(err){
+            console.log('Error..')
+        } else {
+            client.db('Cars').collection('items').insertOne({
+                model: "Ford",
+                year: 2017
+            })
         }
-    ])
+        client.close();
+    })
 });
 
 const port = process.env.PORT || 3001;
