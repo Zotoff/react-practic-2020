3655ac949b4919e86b4d3c4d4f064b30e6cc0a52
Zotoff <paulzotoff@yandex.ru>
2020-06-29 22:23:01 +0300

    Finished mongodb section

--- nodereact/client/src/App.js
+++ nodereact/client/src/App.js
@@ -2,18 +2,81 @@ import React, {Component} from 'react';
 import axios from 'axios';
 
 class App extends Component {
+  state={
+    cars: []
+  }
 
   componentDidMount() {
-    axios.get('http://localhost:3001/api/users')
-        .then((response)=>{
-          console.log(response.data)
-        })
+    // axios.get('http://localhost:3001/api/users')
+    //     .then((response)=>{
+    //       console.log(response.data)
+    //     })
+    this.getCars();
+  }
+
+  getCars(){
+    axios.get('/api/getcars')
+    .then(response => {
+      this.setState({
+        cars: response.data
+      })
+      console.log(response.data)
+    })
+  }
+
+  onCarSubmit = () => {
+    axios.post('/api/addcar', {
+      brand: 'Ford',
+      model: 'Focus',
+      year: 2020,
+      avail: true
+    })
+    .then(response=>{
+      console.log(response.data)
+    })
+  }
+
+  removeCar = () => {
+    axios.post('/api/removecar', {
+      brand: 'Ford'
+    })
+    .then(response=>{
+      this.getCars();
+    })
+  }
+
+  updateCar = () => {
+    axios.post('/api/updatecar', {
+      id: '5efa39ffd43f780c2c8e49ed',
+      brand: 'Kia'
+    })
+    .then(response=>{
+      this.getCars();
+    })
   }
 
   render(){
     return (
         <div className="App">
-          <h1>Hello</h1>
+          <h1>Add Car</h1>
+          <button
+            onClick={()=>this.onCarSubmit()}
+          >Add Car to DB</button>
+          <button
+            onClick={()=>this.removeCar()}
+          >Remove car</button>
+          <button
+            onClick={()=>this.updateCar()}
+          >Update car</button>
+          <hr />
+          {
+            this.state.cars.map((car, i) => (
+              <div key={i}>
+                {car.brand}
+                {car.year}
+              </div>
+            ))
+          }
         </div>
     );
   }
--- nodereact/server.js
+++ nodereact/server.js
@@ -1,31 +1,207 @@
 const express = require('express');
 const {MongoClient} = require('mongodb');
 const app = express();
+const bodyParser = require('body-parser');
 
-const url = 'mongodb://localhost:27017';
+// Grabbing the data from the page
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
 
 
+/*Without Mongoose*/
 
+// const url = 'mongodb://localhost:27017';
 
-app.get('/api/users', (req, res)=>{
-    // res.json([
-    //     {
-    //         name: 'Paul',
-    //         lastname: 'Zotov'
-    //     }
-    // ])
-    MongoClient.connect(url,{useUnifiedTopology: true},(err, client)=>{
-        if(err){
-            console.log('Error..')
-        } else {
-            client.db('Cars').collection('items').insertOne({
-                model: "Ford",
-                year: 2017
-            })
-        }
-        client.close();
+// app.get('/api/users', async (req, res)=>{
+
+//     /*JSON DATA*/
+
+//     // res.json([
+//     //     {
+//     //         name: 'Paul',
+//     //         lastname: 'Zotov'
+//     //     }
+//     // ])
+
+//     /*Single data*/
+   
+//     MongoClient.connect(url,{useUnifiedTopology: true},(err, client)=>{
+//             client.db('Cars').collection('items').insertOne({
+//                 model: "Ford",
+//                 year: 2017
+//             },(err, res)=>{
+//                 if(err){
+//                     console.log(`ERROR: ${err}`)
+//                 } else {
+//                     console.log(`RESPONSE: ${res}`)
+//                 }
+//             });
+//             //client.close();
+//     })
+// });
+
+/*Multiple data*/
+    // const cars = [
+    //     {name: "Ford", year: 2019},
+    //     {name: "Mazda", year: 2010}
+    // ];
+    // app.get('/api/users', (req, res)=>{
+    //     MongoClient.connect(url, {useUnifiedTopology: true}, (err, client)=>{
+    //         if(err){
+    //             console.log('Connection error')
+    //         } else {
+    //             console.log('Connection success');
+    //             client.db('Cars').collection('items').insert(cars,(err, res)=>{
+    //                 if(err) {
+    //                     return console.log(`Error: ${err}`)
+    //                 } else {
+    //                     console.log(res);
+    //                 }
+    //             })
+    //             client.close();
+    //         }
+            
+    //     })
+    // })
+
+/*Getting the info*/
+
+// app.get('/api/find', (req, res)=>{
+//     MongoClient.connect(url, {useUnifiedTopology:true}, (err, client)=>{
+//         /*Simple Find*/
+
+//         // client.db('Cars').collection('items').find().toArray()
+//         // .then((data)=>{
+//         //     console.log(data)
+//         // })
+
+//         /*Ordered find with limit*/
+//         // client.db('Cars').collection('items').find().limit(2).toArray((err, docs)=>{
+//         //     console.log(docs);
+//         // })
+
+//         /*Ordered find with skip*/
+//         client.db('Cars').collection('items').find().skip(1).toArray((err, docs)=>{
+//             console.log(docs);
+//         })
+
+//         client.close()
+//     })
+// });
+
+/*With Mongoose*/
+mongoose.connect('mongodb://localhost:27017/App', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+/*Create Schema and Model*/
+const carSchema = mongoose.Schema({
+    brand: String,
+    model: String,
+    year: Number,
+    avail: Boolean
+});
+
+const Car = mongoose.model('Car', carSchema);
+
+/*Performing the post request*/
+app.post('/api/addcar', (req, res)=>{
+    const addCar = new Car({
+        brand: req.body.brand,
+        model: req.body.model,
+        year: req.body.year,
+        avail: req.body.avail
+    })
+
+    addCar.save((err, doc)=>{
+        if(err) return console.log(err);
+        res.sendStatus(200);
     })
 });
+
+/*Performing the get*/
+app.get('/api/getcars', (req, res)=>{
+    Car.find((err, doc)=>{
+        if(err) return console.log(err);
+        res.json(doc);
+    })
+})
+
+/*Get with params*/
+// app.get('/api/getcars', (req, res)=>{
+//     Car.find({brand: 'Ford'},(err, doc)=>{
+//         if(err) return console.log(err);
+//         res.json(doc);
+//     })
+// })
+
+// app.get('/api/getcars', (req, res)=>{
+//     Car.findbyId('3434r5tfsfsdfsr4534',(err, doc)=>{
+//         if(err) return console.log(err);
+//         res.json(doc);
+//     })
+// })
+
+/*Delete cars*/
+app.post('/api/removecar', (req, res)=>{
+    const brand = req.body.brand;
+    Car.findOneAndRemove({brand: brand}, (err, doc)=>{
+        if(err) return console.log(err);
+        res.json(doc);
+    });
+
+    /*Remove by id*/
+    // const id = req.body.id;
+    // Car.findByIdAndRemove({_id: id}, (err, doc)=>{
+    //     if(err) return console.log(err);
+    //     res.json(doc);
+    // })
+
+    /*Bulk remove by prop*/
+    // const brand = req.body.brand;
+    // Car.remove({brand: brand}, (err, doc)=>{
+    //     if(err) return console.log(err);
+    //     res.json(doc);
+    // })
+
+})
+
+/*Update cars*/
+app.post('/api/updatecar', (req, res)=>{
+    const id = req.body.id;
+    const brand = req.body.brand;
+    
+    Car.update({_id: id}, {$set: {
+        brand: brand
+    }},(err,doc)=>{
+        if(err) return console.log(err);
+        res.json(doc)
+    })
+
+    // Car.findByIdAndUpdate({_id: id}, {$set: {
+    //     brand: brand
+    // }},(err,doc)=>{
+    //     if(err) return console.log(err);
+    //     res.json(doc)
+    // })
+
+    /*Iteraction with the document*/
+    // Car.findById(id, (err, car)=>{
+    //     if(err) return console.log(err);
+
+    //     car.set({
+    //         brand: brand
+    //     });
+    //     car.save((err, doc)=>{
+            //     if(err) return console.log(err);
+            //     res.json(doc)
+            // });
+    // })
+
+})
+
 
 const port = process.env.PORT || 3001;
 
