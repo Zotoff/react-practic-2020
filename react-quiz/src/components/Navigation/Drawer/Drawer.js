3fa8e012924ca569dd8602e7f328bc9d4c9c1a33
Zotoff <paulzotoff@yandex.ru>
2020-07-18 11:33:05 +0300

    Finished quiz first section

---
+++ react-quiz/src/components/Navigation/Drawer/Drawer.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import classes from './Drawer.module.css';
+
+import Backdrop from '../../UI/Backdrop/Backdrop';
+
+const links = [1, 2, 3];
+
+class Drawer extends Component {
+    render(){
+        const cls = [classes.Drawer];
+        if(!this.props.isOpen) {
+            cls.push(classes.close)
+        }
+        return (
+            <React.Fragment>
+                <nav className={cls.join(" ")}>
+                    <ul>
+                        {
+                            links.map((link, index)=>{
+                                return (
+                                    <li key={index}>
+                                        <a href="">{link}</a>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </nav>
+                {this.props.isOpen ? <Backdrop onClick={this.props.onClose}/> : null}
+            </React.Fragment>
+        )
+    }
+}
+
+
+export default Drawer;
